d4588fc9b0e3e1cb2b14291cba557300d5287083
Guto.A <augusto7666@gmail.com>
2023-12-08 09:42:51 -0300

    Organização dos arquivos

--- TrabalhoED2 - Copia/cliente.h
+++
@@ -1,8 +0,0 @@
-typedef struct cliente Cliente;
-typedef struct rota Rota;
-
-Cliente *criarlista();
-Cliente *inserir(Cliente *cliente);
-Cliente *organizar(Cliente *cliente);
-// Rota *organizar(Rota *rota);
-void MostrarTodos(Cliente *cliente);
